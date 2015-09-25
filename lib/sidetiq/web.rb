2bd85d9031ef0df65594dda6375edd9c95f4beb3
Sebastian Johnsson <sebastian@agiley.se>
2015-09-25 20:22:26 +0200

    Remove csrf_tag-helper

--- lib/sidetiq/web.rb
+++ lib/sidetiq/web.rb
@@ -1,14 +1,11 @@
 require 'sidetiq'
 require 'sidekiq/web'
-require 'sidetiq/web_helpers'
 
 module Sidetiq
   module Web
     VIEWS = File.expand_path('views', File.dirname(__FILE__))
 
-    def self.registered(app)
-      app.helpers Sidetiq::WebHelpers
-      
+    def self.registered(app)      
       app.get "/sidetiq" do
         @workers = Sidetiq.workers.sort_by { |worker| worker.name }
         @time = Sidetiq.clock.gettime
--- lib/sidetiq/web_helpers.rb
+++
@@ -1,8 +0,0 @@
-module Sidetiq
-  module WebHelpers
-    # Helper method extracted from Sidekiq (rather than including the entire ::Sidekiq::WebHelpers module)
-    def csrf_tag
-      "<input type='hidden' name='authenticity_token' value='#{session[:csrf]}'/>"
-    end
-  end
-end
